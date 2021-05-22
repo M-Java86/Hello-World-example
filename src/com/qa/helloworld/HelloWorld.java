295eebb6a8e65612cbabe715a6080a3d10637180
M-java86 <mpythondata@outlook.com>
2021-05-22 14:58:58 -0400

    MO:added String, Var and System.out.println"Hello World"

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -2,24 +2,19 @@ package com.qa.helloworld;
 
 //Class has scope
 public class HelloWorld {
-	
 	// Method has a scope
-	public static void printMessage(String message) {
+	public static void main(String [] args) {
 		String exampleVar = "Hello World";
-	     
 		
 		System.out.println(exampleVar);
-	     
-	     
-	 
+	}
 	
 	
-	 }
 }
 
 	
 	 
 //-
-//-Packages walk through, do not create classes in defaultpackage, it is bad pratice. 
+//-Packages walk through, do not create classes in defaultpackage, it is bad practice. 
 
 //-Class members
