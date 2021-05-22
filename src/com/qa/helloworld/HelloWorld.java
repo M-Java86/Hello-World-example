634bc393960f86a47bd06a30198eca1b774506cf
M-java86 <mpythondata@outlook.com>
2021-05-22 16:59:46 -0400

    MO: added Parameters

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -1,34 +1,60 @@
 package com.qa.helloworld;
 
-//Class has scope
-public class HelloWorld {
-	
+
+//Class has scope // it is good practice to indent anything inside of a scope
+//because it helps distinguish that contents has belonging to something above itself and 
+//helps us get a since of which scope belongs to which object or member. 
+//highlight text + TAB-will indent once. 
+
+public class HelloWorld { // A class simply describes attributes or functionality that belongs
+	// to it that can be called.
+
 	public static int id;
 	public static String msg = "Hello World";
-	
-	
-	// Method has a scope
-	public static void main(String [] args) { // special method of class, first thing a Java application will look..
-		//for reference to execute. 
-		//String exampleVar = "Hello World";
+
+	/// Global variable out side the scope.
+
+	// Method has a scope // A Method is design to store and execute code
+	// instructions
+	public static void main(String[] args) { // special method of class, first thing a Java application will look..
 		
-		WelcomeMsg();//-first System operand 
+		//printThese(12, 24);
 		
-		System.out.println(msg);
+		
+		
+		// for reference to execute.
+		// String exampleVar = "Hello World";
+
+		int result;
+
+		result = add(3, 9);
+
+		System.out.println(result);
+
 	}
-	
-	//Bonus Task
-	public static void WelcomeMsg() { // Second System operand
-		System.out.println("Method-Hello World"); // has two system operands, which printed out two "Hello World" in the console.
-		System.out.println("Method-Hello World");
-		System.out.println("Method-Hello World");
+
+	public static void printThese(int num1, int num2 ) {
+		
+		System.out.println(num1);
+		System.out.println(num2);
 	}
-	
+
+	{
+
+	}
+
+	public static int add(int num1, int num2) { // Method that returns a value
+		return num1 + num2; // a keyword is Java .
+
+	}
+
 }
 
-	
-	 
 //-
 //-Packages walk through, do not create classes in defaultpackage, it is bad practice. 
 
 //-Class members
+
+//-Scope Ownership formatting
+//syso Ctrl + space print method
+//Ctrl + shift + f -  auto indent 
