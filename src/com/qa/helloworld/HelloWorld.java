91f9eb9091f1e7b2da995b4454d0e48673c3da10
M-java86 <mpythondata@outlook.com>
2021-05-22 14:33:01 -0400

    MO:inital commit

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.qa.helloworld;
+
+//Class has scope
+public class HelloWorld {
+	
+	// Method has a scope
+	public static void printMessage(String message) {
+		String exampleVar = "Hello World";
+	     
+		
+		System.out.println(exampleVar);
+	     
+	     
+	 
+	
+	
+	 }
+}
+
+	
+	 
+//-
+//-Packages walk through, do not create classes in defaultpackage, it is bad pratice. 
+
+//-Class members
